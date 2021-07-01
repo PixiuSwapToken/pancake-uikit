7b66f75541865e110a283be664e88e6ec5afb7a5
pixiuswap <83927033+pixiuswap@users.noreply.github.com>
2021-07-01 04:39:27 +0300

    Initial commit

---
+++ src/widgets/Menu/components/LangSelector.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { SvgProps } from "../../../components/Svg";
+import Text from "../../../components/Text/Text";
+import Dropdown from "../../../components/Dropdown/Dropdown";
+import Button from "../../../components/Button/Button";
+import * as IconModule from "../icons";
+import { LangType } from "../types";
+import MenuButton from "./MenuButton";
+
+const Icons = (IconModule as unknown) as { [key: string]: React.FC<SvgProps> };
+const { LanguageIcon } = Icons;
+
+interface Props {
+  currentLang: string;
+  langs: LangType[];
+  setLang: (lang: LangType) => void;
+}
+
+const LangSelector: React.FC<Props> = ({ currentLang, langs, setLang }) => <></>;
+
+export default React.memo(LangSelector, (prev, next) => prev.currentLang === next.currentLang);
